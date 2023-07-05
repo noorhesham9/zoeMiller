ac6e9dc96127506a5d66e3681b411f266b2b2d31
norhs <norhsham02@gmail.com>
2023-07-05 22:17:43 +0300

    finish servics

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,64 +9,66 @@ import { FaBehance } from "react-icons/fa";
 const Home = () => {
   return (
     <section className="home" id="home">
-      <div className="home__container container">
-        <p className="home__subtitle text-cs">
-          Hello, <span>My Name Is</span>
-        </p>
+      <div className="home__wrapper">
+        <div className="home__container container">
+          <p className="home__subtitle text-cs">
+            Hello, <span>My Name Is</span>
+          </p>
 
-        <h1 className="home__title text-cs">
-          <span>ZOÉ</span> MILLER
-        </h1>
-        <p className="home__jop">
-          <span className="text-cs">I Am</span> <b>Web Develober</b>
-        </p>
+          <h1 className="home__title text-cs">
+            <span>ZOÉ</span> MILLER
+          </h1>
+          <p className="home__jop">
+            <span className="text-cs">I Am</span> <b>Web Develober</b>
+          </p>
 
-        <div className="home__img-wrapper">
-          <div className="home__banner">
-            <img src={profileImg} alt="" className="home__profile" />
+          <div className="home__img-wrapper">
+            <div className="home__banner">
+              <img src={profileImg} alt="" className="home__profile" />
+            </div>
+            <p className="home__data home__data-one">
+              <span className="text-lg">
+                12 <b>+</b>
+              </span>
+              <span className="text-sm text-cs">
+                Years of <span>Experience</span>
+              </span>
+            </p>
+            <p className="home__data home__data-two">
+              <span className="text-lg">330</span>
+              <span className="text-sm text-cs">
+                Completed <span>Projects</span>
+              </span>
+            </p>
+
+            <img src={ShapeOne} alt="" className="shape shape__1" />
+            <img src={ShapeTwo} alt="" className="shape shape__2" />
+            <img src={ShapeTwo} alt="" className="shape shape__3" />
           </div>
-          <p className="home__data home__data-one">
-            <span className="text-lg">
-              12 <b>+</b>
-            </span>
-            <span className="text-sm text-cs">
-              Years of <span>Experience</span>
-            </span>
-          </p>
-          <p className="home__data home__data-two">
-            <span className="text-lg">330</span>
-            <span className="text-sm text-cs">
-              Completed <span>Projects</span>
-            </span>
-          </p>
 
-          <img src={ShapeOne} alt="" className="shape shape__1" />
-          <img src={ShapeTwo} alt="" className="shape shape__2" />
-          <img src={ShapeTwo} alt="" className="shape shape__3" />
-        </div>
-
-        <p className="home__text">
-          From France, Paris. I have rich expoerince in web design , also Iam
-          good at wordpress. Iove to talk with you about unique
-        </p>
-        <div className="home__socials">
-          <a href="" className="home__socials-link">
-            <FaTwitter />
-          </a>
-          <a href="" className="home__socials-link">
-            <FaDribbble />
-          </a>
-          <a href="" className="home__socials-link">
-            <FaBehance />
-          </a>
-        </div>
-        <div className="home__btns">
-          <a href="" className="btn text-cs">
-            Download
-          </a>
-          <a href="" className="hero__link text-cs">
-            My Skills
-          </a>
+          <p className="home__text">
+            From France, Paris. I have rich expoerince in web design , also Iam
+            good at wordpress. Iove to talk with you about unique
+          </p>
+          <div className="home__socials">
+            <a href="" className="home__socials-link">
+              <FaTwitter />
+            </a>
+            <a href="" className="home__socials-link">
+              <FaDribbble />
+            </a>
+            <a href="" className="home__socials-link">
+              <FaBehance />
+            </a>
+          </div>
+          <div className="home__btns">
+            <a href="" className="btn text-cs">
+              Download
+            </a>
+            <a href="" className="hero__link text-cs">
+              My Skills
+            </a>
+          </div>
         </div>
       </div>
     </section>
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,58 @@
+import { services } from "../../Data";
+import { FaArrowRight } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import shapeTwo from "../../assets/shape-2.png";
+import "./services.css";
+
+const Services = () => {
+  return (
+    <section className="services section" id="services">
+      <h2 className="section__title text-cs">What I DO</h2>
+      <p className="section__subtitle text-cs">
+        My <span>Services</span>
+      </p>
+
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          540: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1200: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+        }}
+        modules={[Pagination]}
+        className="services__container container mySwiper">
+        {services.map(({ name, title, description }, index) => {
+          return (
+            <SwiperSlide key={index} className="services__item card card-one">
+              <span className="services__subtitle text-cs">{name}</span>
+              <h3 className="services__title">{title}</h3>
+              <p className="services__description">{description}</p>
+              <a href="" className="link">
+                See Pricing <FaArrowRight className="link__icon" />
+              </a>
+              <img src={shapeTwo} alt="" className="shape c__shape" />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Services;
