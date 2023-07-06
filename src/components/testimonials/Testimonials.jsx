0cdf526c7a73adc54909fe4f9e1572d877e9b9e3
norhs <norhsham02@gmail.com>
2023-07-06 20:03:42 +0300

    finish testimonials

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,63 @@
+import "./testimoials.css";
+import shapeTwo from "../../assets/shape-2.png";
+import { testimonials } from "../../Data";
+import testimonialIcon from "../../assets/testimonials-icon.svg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+const Testimonials = () => {
+  return (
+    <section className="testimonials section" id="testimonials">
+      <h2 className="section__title text-cs">Testimonials</h2>
+      <p className="section__subtitle text-cs">
+        My <span>Customers Say</span>
+      </p>
+
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          540: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1200: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+        }}
+        modules={[Pagination]}
+        className="testimonials__container container mySwiper">
+        {testimonials.map(({ img, name, author, description }, index) => {
+          return (
+            <SwiperSlide
+              key={index}
+              className="testimonials__item card card-one">
+              <div className="testimonial__header">
+                <div className="testimonial__icon">
+                  <img src={testimonialIcon} alt="" />
+                </div>
+                <img src={img} alt="" className="testimonial__img" />
+              </div>
+              <p className="testimonial__description">{description}</p>
+              <h3 className="testimonial__name">{name}</h3>
+              <p className="testimonial__author">{author}</p>
+
+              <img src={shapeTwo} alt="" className="shape c__shape" />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonials;
